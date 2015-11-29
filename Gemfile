f4a16bce3994d0c69d23c00193dcfdc61d65dc62
Tamrin007 <tamrin.bitterchococo@gmail.com>
2015-11-29 12:04:47 +0900

    server setting

--- Gemfile
+++ Gemfile
@@ -6,6 +6,17 @@ gem 'therubyracer'
 gem "activerecord"
 gem "mysql2"
 
+# capistrano
+gem 'capistrano', '~> 3.2.1'
+gem 'capistrano-rails'
+gem 'capistrano-rbenv'
+gem 'capistrano-bundler'
+gem 'capistrano3-unicorn'
+
+group :production do
+  gem 'unicorn'
+end
+
 group :develoopment do
   gem 'shotgun'
   gem 'thin'
---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+lock '3.2.1'
+
+set :application, 'ARCoupon_Web' # アプリケーション名
+set :repo_url, 'git@github.com:Tamrin007/ARCoupon_Web.git' # レポジトリのSSH clone URL
+set :branch, 'master' # デプロイしたいブランチ名
+set :deploy_to, '/var/www/vhosts/sinatra/ARCoupon_Web' # デプロイ先サーバのディレクトリ
+set :scm, :git
+set :log_level, :debug
+set :pty, true
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
+set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
